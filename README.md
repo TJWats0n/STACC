82c08c11a3e605fb095663e7c509c1ac7285fe41
TJWats0n <cjuliankopp@gmail.com>
2018-06-14 09:43:16 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-# DUB2018
-Spatiotemporal event detection from twitter stream data
-=======
-# STACC
->>>>>>> origin/master
+# STACC Spatio Temporal Anomalous Crowd Contextualisation
+
+This is the code repository for STACC - a system which detects abnormal crowded places and the infers background information on why the place is crowded. STACC was created by Julian Kopp within the scope of his Bachelor Thesis, supervised by Haytham Assem from IBM's Innvoation Exchange Ireland Team.
+
--- real_time_config.py
+++ real_time_config.py
@@ -1,11 +1,11 @@
 class RTConfig:
     #Database credentials
     cloudant_cred = {
-        "username": "0a9dac16-96d4-4ea9-b41a-62bcd51de0f5-bluemix",
-        "password": "c8490e7b530803b38044e6fcb9b92903aa178a22f9cd9f214f1d372d5e5993a3",
-        "host": "0a9dac16-96d4-4ea9-b41a-62bcd51de0f5-bluemix.cloudant.com",
+        "username": "your_cred_here",
+        "password": "your_cred_here",
+        "host": "your_cred_here",
         "port": 443,
-        "url": "https://0a9dac16-96d4-4ea9-b41a-62bcd51de0f5-bluemix:c8490e7b530803b38044e6fcb9b92903aa178a22f9cd9f214f1d372d5e5993a3@0a9dac16-96d4-4ea9-b41a-62bcd51de0f5-bluemix.cloudant.com"
+        "url": "your_cred_here"
     }
 
     selector = {
@@ -23,9 +23,9 @@ class RTConfig:
     #Twitter API keys
     keys = {
         'jk': {
-            'ckey': 'xdACHwFupBPsFDGmsF6H7HKNU',
-            'csecret': '6ZBryD8NrtGSWiLDfe2XYmj1f8jzEXkpzgSkEjKqbhxTlUtBIQ',
-            'atoken': '2865752009-r766x9fKZx0kBY4MCAEuIE5T4EYmkjvEYc77n1q',
-            'asecret': 'EXVhwGJjsPGnmDS7TWZ2ICu7vilJeYIpNgQjMhaF9YicA',
+            'ckey': 'your_key_here',
+            'csecret': 'your_key_here',
+            'atoken': 'your_key_here',
+            'asecret': 'your_key_here',
         }
     }
